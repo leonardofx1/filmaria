359abf1226caa7ab7771c88be66c2dd707e5d364
leonardofx1 <leonardonunesfreitas1@gmail.com>
2023-03-19 21:28:27 -0300

    animacao

--- src/components/CardMovie/index.jsx
+++ src/components/CardMovie/index.jsx
@@ -1,14 +1,16 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { MdGrade} from "react-icons/md";
 import movieCard from '../../assets/style/movieCard.scss'
 
-const CardMovie = ({title, vote_average, poster_path, id}) => {
+const CardMovie = ({title, poster_path, id, vote_average}) => {
     const apiImg = 'https://image.tmdb.org/t/p/w500/'
 
     return (
         <>
             <ul className='card__corpo'>
-                <li className='card__img'><img src={apiImg + poster_path} alt={title} /><span>{vote_average}</span> </li>
+                <li className='card__img'><img src={apiImg + poster_path} alt={title} /> <span className='votos' > <MdGrade/> {vote_average}</span> </li>
                 <li>{title}</li>
+               
                 <Link to={`/movie/${id}`}className='detalhes' >ver detalhes</Link>
             </ul>
         </>
